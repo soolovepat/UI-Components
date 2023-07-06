e1c46b91e022756d2193300b3df56e60d7bbd17e
Sujin Lee <soolovepat@naver.com>
2023-07-06 18:10:02 +0900

    Add icon

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { ChevronRight } from "react-bootstrap-icons";
 
 const Button = () => {
-  return <div>Button</div>;
+  return (
+    <div>
+      <h1>Button</h1>
+      <button>
+        Large Primary Button <ChevronRight color="" size={14} />
+      </button>
+    </div>
+  );
 };
 
 export default Button;
