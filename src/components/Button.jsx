69c3ac349a5b8b410ce5eaa8265f208d0ad4ed59
Sujin Lee <soolovepat@naver.com>
2023-07-06 19:18:59 +0900

    Add onClick

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,7 +9,7 @@ const Button = () => {
       <StButton borderColor="#55efc4" backgroundColor="#55efc4">
         <button
           className="large-button"
-          onClick={() => alert("버튼을 만들어보세요")}
+          onClick={() => alert("알림이 설정되었습니다")}
         >
           Large Primary Button <ChevronRight color="" size={12} />
         </button>
@@ -17,7 +17,10 @@ const Button = () => {
         <button className="small-button">Small</button>
       </StButton>
       <StButton borderColor="#fab1a0" backgroundColor="#fab1a0" color="#d63031">
-        <button className="large-button">
+        <button
+          className="large-button"
+          onClick={() => prompt("암호를 입력해주세요")}
+        >
           Large Negative Button <ChevronRight color="" size={12} />
         </button>
         <button className="medium-button">Medium</button>
