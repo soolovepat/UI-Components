4adab77ff93d533884dde8186a55f0e52e0284b3
Sujin Lee <soolovepat@naver.com>
2023-07-06 17:39:07 +0900

    Add components file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import Modal from "./components/Modal";
+import Select from "./components/Select";
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  return (
+    <div>
+      <Button />
+      <Input />
+      <Modal />
+      <Select />
+    </div>
+  );
+};
 
 export default App;
