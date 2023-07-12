6b4464210f460fb502210abf3f3484e6b863c829
Sujin Lee <soolovepat@naver.com>
2023-07-12 17:21:34 +0900

    Update Overlay

--- src/components/Modal/ModalWindow.jsx
+++ src/components/Modal/ModalWindow.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { ModalBackground, StModalWindow } from "../../styles/ModalWindowStyle";
+import { Overlay, ModalContainer, Modal } from "../../styles/ModalStyle";
 
 const ModalWindow = ({ openModalHandler, confirmButton }) => {
   return (
-    <ModalBackground>
-      <StModalWindow>
+    <ModalContainer>
+      <Overlay />
+      <Modal>
         <p>
           닫기와 확인 버튼 2개가 있고, 외부 영역을 눌러도 모달이 닫히지 않아요.
         </p>
         <button onClick={openModalHandler}>닫기</button>
         <button onClick={confirmButton}>확인</button>
-      </StModalWindow>
-    </ModalBackground>
+      </Modal>
+    </ModalContainer>
   );
 };
 
