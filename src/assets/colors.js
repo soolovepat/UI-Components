93f17b3654af33ec2f0daa0dfe6f2f78e8069eea
Sujin Lee <soolovepat@naver.com>
2023-07-13 00:53:05 +0900

    Add pinkColor

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -1,2 +1,3 @@
 export const blueColor = ["#3b38eb", "#716ef9", "#cdcdff"];
-export const grayColor = ["#fff", "#000"];
+export const pinkColor = ["#edd0f6", "#ff9046"];
+export const grayColor = ["#fff", "#4e4e4e", "#000"];
