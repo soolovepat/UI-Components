ba681ad7bbba6d194e179b0a1b03697e79cb0f35
Sujin Lee <soolovepat@naver.com>
2023-07-12 17:21:49 +0900

    Add Select

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
 const Select = () => {
+  const OPTIONS = [
+    { value: "사과" },
+    { value: "바나나" },
+    { value: "오렌지" },
+    { value: "키위" },
+    { value: "블루베리" },
+  ];
   return (
     <div>
       <h1>Select</h1>
+      <select defaultValue={"바나나"}>
+        {OPTIONS.map((option, idx) => {
+          return (
+            <option key={idx} value={option.value}>
+              {option.value}
+            </option>
+          );
+        })}
+      </select>
     </div>
   );
 };
