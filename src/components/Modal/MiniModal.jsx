cec6990a6904838f5aa868668da1caefc44c4f63
Sujin Lee <soolovepat@naver.com>
2023-07-12 17:21:11 +0900

    Add MiniModal

--- src/components/Modal/MiniModal.jsx
+++ src/components/Modal/MiniModal.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Overlay, ModalContainer, Modal } from "../../styles/ModalStyle";
 
-const MiniModal = () => {
-  return <div>MiniModal</div>;
+const MiniModal = ({ openMiniModalHandler }) => {
+  return (
+    <ModalContainer>
+      <Overlay onClick={openMiniModalHandler} />
+      <Modal>
+        <p>
+          닫기 버튼 1개가 있고,
+          <br />
+          외부 영역을 누르면 모달이 닫혀요.
+        </p>
+        <button onClick={openMiniModalHandler}>X</button>
+      </Modal>
+    </ModalContainer>
+  );
 };
 
 export default MiniModal;
