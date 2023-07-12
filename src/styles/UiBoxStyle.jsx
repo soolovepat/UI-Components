40e23b167bee078a858fc87735f0f34048592cf7
Sujin Lee <soolovepat@naver.com>
2023-07-12 20:40:25 +0900

    Add Style

--- src/styles/UiBoxStyle.jsx
+++ src/styles/UiBoxStyle.jsx
@@ -1,5 +1,11 @@
 import { styled } from "styled-components";
 
 export const UiBoxStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  border-radius: 20px;
+  box-shadow: 4px 12px 30px 6px rgba(0, 0, 0, 0.09);
+
   background-color: #fff;
 `;
