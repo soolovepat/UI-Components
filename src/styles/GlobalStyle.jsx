70b0b59af0bf0c0af86cec7c3112b74ad2b26310
Sujin Lee <soolovepat@naver.com>
2023-07-12 20:26:26 +0900

    Move index.css to GlobalStyle.jsx

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,8 +1,29 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    background-color: #f3f3f3;
+    
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+
   main {
-    margin: 8px
+    display: flex;
+    justify-content: center;
+    margin: 8px;
+
+    
     }
 `;
 
