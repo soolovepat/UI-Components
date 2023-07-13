804b0a2057311ed42a4b3e08b1eda4488a605a32
Sujin Lee <soolovepat@naver.com>
2023-07-13 15:11:54 +0900

    Fix select box

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import UiBox from "../Container/UiBox";
+import { CaretDownFill } from "react-bootstrap-icons";
+import Button from "./Button";
 
 const Select = () => {
   const OPTIONS = [
@@ -9,17 +11,39 @@ const Select = () => {
     { value: "키위" },
     { value: "블루베리" },
   ];
+  const [selectList, setSelectList] = useState(OPTIONS[0].value);
+  const [optionList, setOptionList] = useState(false);
+
+  const onClickSelectBox = () => {
+    setOptionList(!optionList);
+  };
+
+  const onClickList = (option) => {
+    setSelectList(option);
+    setOptionList(!optionList);
+  };
+
   return (
     <UiBox title={"Select"}>
-      <select defaultValue={"바나나"}>
-        {OPTIONS.map((option, idx) => {
-          return (
-            <option key={idx} value={option.value}>
-              {option.value}
-            </option>
-          );
-        })}
-      </select>
+      <Button onClick={onClickSelectBox} size={"large"} btn_style={"primary"}>
+        {selectList}
+        <CaretDownFill color="" size={12} />
+      </Button>
+      {optionList && (
+        <ul defaultValue={"바나나"}>
+          {OPTIONS.map((option, idx) => {
+            return (
+              <li
+                onClick={() => onClickList(option.value)}
+                key={idx}
+                value={option.value}
+              >
+                {option.value}
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </UiBox>
   );
 };
